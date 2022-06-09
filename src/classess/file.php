56837386491a0ff9f38fbbdd2c2e76156c7dc4e5
Ignacio Núñez <JefaturaEstudios@itecfp.com>
2022-06-09 10:32:58 +0200

    Nueva lógica de escritura y lectura

--- src/classess/file.php
+++ src/classess/file.php
@@ -3,24 +3,16 @@
     use Exception;
     class file{
         private string $filename;
-        private $file;
         private string $content;
 
 
         public function __construct(string $filename){
             $this->filename = $filename;
-            $this->file = fopen($this->filename, "w");
-            if(!$this->filename){
-                throw new Exception("Error al crear el archivo");
-            }
-            $this->readFile();
+            $this->content = file_get_contents($this->filename);    
         }
         
-        public function openOrCreateFile(string $file):file{
-           return new file($file);
-        }
-        public function readFile():void{
-            $this->content = filesize($this->filename)>0?fread($this->file, filesize($this->filename)):"";
+        public function openOrCreateFile(string $filename):file{
+           return new file($filename);
         }
         public function getContent():string{
             return $this->content;
@@ -31,7 +23,6 @@
         public function writeFile(){
             if(!file_exists($this->filename))
                 throw new Exception("El archivo no existe");
-            fwrite($this->file, $this->content);
-            fclose($this->file);
+            file_put_contents($this->filename, $this->content);
         }
     }    
